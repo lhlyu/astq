03cd91d23c9ede46defc9b61978bfba57a4fd569
lhlyu <misakafs@gmail.com>
2022-05-28 18:06:44 +0800

    update request method

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,31 +4,23 @@ import useConfigStore from '../stores/config'
 // 查询所有的订阅情况
 export const GetSubscriptions = async () => {
     const store = useConfigStore()
-    return instance.get('/api/subscriptions', {
-        data: JSON.stringify(store.getReq)
-    })
+    return instance.post('/api/subscriptions', store.getReq)
 }
 
 // 查询订单信息
 export const GetLookupOrder = async () => {
     const store = useConfigStore()
-    return instance.get('/api/lookup', {
-        data: JSON.stringify(store.getReq)
-    })
+    return instance.post('/api/lookup', store.getReq)
 }
 
 // 查询退款信息
 export const GetRefund = async () => {
     const store = useConfigStore()
-    return instance.get('/api/refund', {
-        data: JSON.stringify(store.getReq)
-    })
+    return instance.post('/api/refund', store.getReq)
 }
 
 // 查询历史交易信息
 export const GetHistory = async () => {
     const store = useConfigStore()
-    return instance.get('/api/history', {
-        data: JSON.stringify(store.getReq)
-    })
+    return instance.post('/api/history', store.getReq)
 }
